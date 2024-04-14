e9eef78ceef3b8a73c34f4c0ab5e3386f18ed4e5
myPC <user@DESKTOP-VE36L8M>
2024-04-14 07:49:45 +0300

    init

--- s21_div.c
+++ s21_div.c
@@ -3,7 +3,9 @@
 int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result){
     int error=0;
     nullify(result);
-    s21_decimal one={{0b01,0,0,0}};
+    s21_decimal two={{0b10,0,0,0}};
+    *result=two;
+int count=0;
     int sign1=extractBitSign(value_1);
     int sign2=extractBitSign(value_2);
     unsigned int sign=0;  
@@ -12,11 +14,17 @@ s21_big_decimal big1, big2, resbig, buffer;
      int scale=normalize(value_1, value_2, &big1, &big2);
      s21_decimal compareit; nullify(&compareit);
      while(s21_is_less(value_2, value_1)){
-       myshiftlefts(&value_2, 1);
-       myadd(*result, one, result);
-
+      
+       count++;
+myshiftlefts(&value_2, 1);
 
      }
+     while(count>0){
+        myshiftlefts(&two, 1);
+       // my_mul_no_normalize(*result, two, result);
+        count--;
+       *result=two;
+}
 }
 
 int div_int(unsigned int value1, unsigned int value2, unsigned int* result){
